60e7a347c310fbf0e485a7334f54242381796d62
dejana023 <dejana023@gmail.com>
2023-03-31 11:52:58 +0200

    feat - fluent validator has been completed

---
+++ CashRegister.API/Validators/ProductBillValidator.cs
@@ -0,0 +1,13 @@
+﻿using CashRegister.Domain.DTO;
+using FluentValidation;
+
+namespace CashRegister.API.Validators
+{
+    public class ProductBillValidator : AbstractValidator<ProductBillPostPutDTO>
+    {
+        public ProductBillValidator() {
+            RuleFor(productBill => productBill.ProductQuantity).NotEmpty().WithMessage("Please add a product quantity");
+        }
+
+    }
+}
